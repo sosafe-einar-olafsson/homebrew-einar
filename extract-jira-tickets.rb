523e28b76983686c24e832b7498457346c667654
Einar Ólafsson <einar.olafsson@sosafe.de>
2024-10-30 17:01:06 +0100

    fix version and remove script

--- extract-jira-tickets.rb
+++ extract-jira-tickets.rb
@@ -1,9 +1,9 @@
 class ExtractJiraTickets < Formula
   desc "Extract Jira tickets from git history between two tags"
   homepage "https://github.com/sosafe-einar-olafsson/extract-jira-tickets"
-  url "https://github.com/sosafe-einar-olafsson/homebrew-extract-jira-tickets/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c74ff90cadd81f27d9d8eb9713daf1870056d8a86c3fa8bffa455ee6d6d87e0d"
-  version "1.0"
+  url "https://github.com/sosafe-einar-olafsson/extract-jira-tickets/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "f05d82ea1795bd4b85a52c60b2162acebf1b1dc3384995f1cc6677eb47b4d16c"
+  version "0.1.0"
 
   def install
     bin.install "extract-jira-tickets"  # Update to match your script name
