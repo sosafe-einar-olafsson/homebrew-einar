c61c5e778472ba809cfb15113d80f23a2fc7f1ab
Einar Ólafsson <einar.olafsson@sosafe.de>
2024-10-30 16:51:53 +0100

    fix sha256

--- extract-jira-tickets.rb
+++ extract-jira-tickets.rb
@@ -1,8 +1,8 @@
 class ExtractJiraTickets < Formula
   desc "Extract Jira tickets from git history between two tags"
   homepage "https://github.com/sosafe-einar-olafsson/extract-jira-tickets"
-  url "https://github.com/sosafe-einar-olafsson/extract-jira-tickets/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "your_tarball_sha256_hash_here"
+  url "https://github.com/sosafe-einar-olafsson/homebrew-extract-jira-tickets/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "c74ff90cadd81f27d9d8eb9713daf1870056d8a86c3fa8bffa455ee6d6d87e0d"
   version "1.0"
 
   def install
